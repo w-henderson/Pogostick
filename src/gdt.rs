86ca610db1b1aa768c19c7c4ded646d9c507093c
William Henderson <william-henderson@outlook.com>
2021-03-02 20:43:12 +0000

    Improved GDT by loading more required registers

--- src/gdt.rs
+++ src/gdt.rs
@@ -13,6 +13,7 @@ pub const DOUBLE_FAULT_IST_INDEX: u16 = 0;
 struct Selectors {
     code_selector: SegmentSelector,
     tss_selector: SegmentSelector,
+    data_selector: SegmentSelector,
 }
 
 lazy_static! {
@@ -34,11 +35,13 @@ lazy_static! {
     static ref GDT: (GlobalDescriptorTable, Selectors) = {
         let mut gdt = GlobalDescriptorTable::new();
         let code_selector = gdt.add_entry(Descriptor::kernel_code_segment());
+        let data_selector = gdt.add_entry(Descriptor::kernel_data_segment());
         let tss_selector = gdt.add_entry(Descriptor::tss_segment(&TSS));
         (
             gdt,
             Selectors {
                 code_selector,
+                data_selector,
                 tss_selector,
             },
         )
@@ -46,11 +49,16 @@ lazy_static! {
 }
 
 pub fn init() {
-    use x86_64::instructions::{segmentation::set_cs, tables::load_tss};
+    use x86_64::instructions::{segmentation, tables};
 
     GDT.0.load();
     unsafe {
-        set_cs(GDT.1.code_selector);
-        load_tss(GDT.1.tss_selector);
+        segmentation::set_cs(GDT.1.code_selector);
+        segmentation::load_ds(GDT.1.data_selector);
+        segmentation::load_es(GDT.1.data_selector);
+        segmentation::load_fs(GDT.1.data_selector);
+        segmentation::load_gs(GDT.1.data_selector);
+        segmentation::load_ss(GDT.1.data_selector);
+        tables::load_tss(GDT.1.tss_selector);
     }
 }
