1dc9ddc262e71de593504fc36c6d313e521173e0
William Henderson <william-henderson@outlook.com>
2021-03-05 20:43:51 +0000

    Updated build script to detect QEMU_PATH env var

--- build.sh
+++ build.sh
@@ -5,4 +5,9 @@ qemu-img create disk.img 32M
 echo "copying kernel"
 dd conv=notrunc if=target/pogostick/debug/bootimage-pogostick.bin of=disk.img
 echo "starting emulator"
-qemu-system-x86_64 -L D:\\Programs\\qemu -hdc "disk.img"
+
+if [[ -z "${QEMU_PATH}" ]]; then
+  qemu-system-x86_64 -hdc "disk.img"
+else
+  qemu-system-x86_64 -L "${QEMU_PATH}" -hdc "disk.img"
+fi
